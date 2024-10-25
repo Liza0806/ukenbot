ab517819969d34d45052c75188e3a5227b257898
Liza <fildepiorsta@gmail.com>
2024-10-25 11:45:07 +0300

    f

--- handlers/getTodaySchedule.js
+++ handlers/getTodaySchedule.js
@@ -1,20 +1,23 @@
-const { Event } = require("../models/eventModel");
+  const { Event } = require("../models/eventModel");
 
 const getTodaySchedule = async (ctx) => {
-    const userId = ctx.from.id.toString();
-    const now = new Date();
-    const startOfDay = new Date(now.getDay(), now.getDay(), 1); //начало дня 
-    const endOfDay = new Date(now.getDay(), now.getDay() + 1, 0); // конец дня
-    const events = await Event.find({ date: { $gte: startOfDay, $lte: endOfDay }})
-    if (events.length === 0) {
-      const reply = await ctx.reply("Костя сегодня свободен!");
-      return;
-    }
-    let scheduleMessage = "Вот тренировки на сегодня:\n";
-    events.forEach(event => {
-      scheduleMessage += `- ${event.date}: ${event.groupTitle}\n`; 
-    });
+  const now = new Date();
+  const startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate()); // начало дня
+  const endOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1); // конец дня
+
+  const events = await Event.find({ date: { $gte: startOfDay, $lt: endOfDay } });
   
-    await ctx.reply(scheduleMessage);
+  if (events.length === 0) {
+    await ctx.reply("Костя сегодня свободен!");
+    return;
   }
-  module.exports = { getTodaySchedule };
+
+  let scheduleMessage = "Вот тренировки на сегодня:\n";
+  events.forEach(event => {
+    scheduleMessage += `- ${event.date}: ${event.groupTitle}\n`; 
+  });
+  
+  await ctx.reply(scheduleMessage);
+};
+
+module.exports = { getTodaySchedule };
